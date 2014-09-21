4a86c3ef1fcca5e9cbce2589ba237498d170d64f
Sean Kirby <seank@nulogy.com>
2014-09-21 17:33:43 -0400

    Allows showing of articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def new
-
   end
 
   def create
@@ -8,6 +7,10 @@ class ArticlesController < ApplicationController
 
     @article.save
     redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
